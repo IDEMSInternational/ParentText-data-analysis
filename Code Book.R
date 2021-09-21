b3f10c51eae40fa8f5e338990c297c05cb62115a
lilyclements <lclements1004@outlook.com>
2021-09-21 15:14:51 +0100

    Update Code Book.R

--- Code Book.R
+++ Code Book.R
@@ -3,16 +3,37 @@ library(httr)
 library(jsonlite)
 library(tidyverse)
 
-setwd("C:/Users/ATEAMATE/Downloads/ParentText-data-analysis-master")
+# 1. Package environment ------------------------------------------------
+# for this to work you need to change the directory to where the token key is stored.
+key <- read.table("C:/Users/lzc1n17/OneDrive - University of Southampton/PhD/IDEMS/ParentText/PT_malaysia_key.txt", quote="\"", comment.char="")
+set_rapidpro_key(key = key)
+set_rapidpro_site(site = "https://rapidpro.idems.international/api/v2/")
+set_rapidpro_uuid_names()
+get_rapidpro_uuid_names()
 
-# 1. Reading in from RapidPro --------------------------------------------------------------------------------------
-uuid_data <- read.csv("./Files/flows_uuids.csv")
-token <- read.table("token.txt", quote="\"", comment.char="")
+# 2. Getting the data in -----------------------------------------------------
+get_user_data()
+#get_rapidpro_uuid_names() %>% dplyr::filter(grepl("PLH - Supportive", name))
+flow_name <- "PLH - Supportive - Development"
+flow_interaction <- get_flow_data(flow_name = flow_name)
+# TODO: result1 is the name. Need to get the result names.
+# At the moment the names are "result1", "result", or more descriptive relating to the flow name
+# Would they always relate to the flow name?
+# for "Supportive - Calm" we have "result_1" and "skill". What is the difference between them?
 
-user_data <- get_user_data(token)
+# 3. Information at user level -----------------------------------------------
+# list of variable names
+user_data <- get_user_data()
+user_data_all_vars <- lapply(user_data, names)
 
-flow_name <- "PLH - Supportive - Praise"
-flow_interaction <- get_flow_data(uuid_data,token, flow_name, result_1)
-response_rate_graphs(flow_interaction, flow_name)
+# contact list values - get variables that contain a set word
+user_data1 <- select(user_data, matches("survey"))
 
+names(get_user_data())
 
+# user data
+get_user_group_data()
+
+# 4. Information at flow level -----------------------------------------------
+response_rate_graphs(flow_interaction = flow_interaction)
+flow_interaction <- get_flow_data(flow_name = "PLH - Supportive - Development")
