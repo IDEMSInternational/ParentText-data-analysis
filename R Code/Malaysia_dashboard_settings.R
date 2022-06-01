4f5a660a2e36593a1088f73ee3cd76cce0f074af
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-06-01 16:09:21 +0200

    Create Malaysia_dashboard_settings.R

---
+++ R Code/Malaysia_dashboard_settings.R
@@ -0,0 +1,23 @@
+#data cleaning
+"tokens/PT_malaysia_key.txt"
+site = "https://app.rapidpro.io/api/v2/"
+include_archived_data
+date_from = "2021-10-14"
+supportive_flow_names <- c("PLH - Supportive - Family", "PLH - Supportive - Help reminder", "PLH - Supportive - Share", "PLH - Supportive - Share - Enrollment",
+                           "GG - PLH - Supportive - Share - Enrollment", "PLH - Supportive - Budget", "PLH - Supportive - Activities for babies", "PLH - Supportive - Activities",
+                           "PLH - Supportive - Behave reminder", "PLH - Supportive - Children reminder", "PLH - Supportive - Covid", "PLH - Supportive - Development",
+                           "PLH - Supportive - Disabilities")
+"GG - PLH - Supportive - Share - Enrollment"
+prefix
+MY
+JM
+PH
+SA
+
+#shiny template
+skin <- "blue"
+background <- "light-blue"
+status <- "primary"
+flag?
+  
+@fields.state_of_origin  #integers
