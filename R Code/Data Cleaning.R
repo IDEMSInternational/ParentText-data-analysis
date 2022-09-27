6383cb217aa6013c06d9656f2ac470ca5d7370bb
lilyclements <lily@idems.international>
2022-09-27 11:32:22 +0100

    Update Data Cleaning.R

--- R Code/Data Cleaning.R
+++ R Code/Data Cleaning.R
@@ -462,7 +462,7 @@ update_data <- function(country = "Malaysia", date_from = "2021-10-14", date_to 
   df_created_on$row <- NULL
   
   
-  if (!country %in% c("Malaysia", "Philippines", "Jamaica")){
+  if (!country %in% c("Malaysia", "Philippines", "Jamaica", "South Africa", "South_Africa")){
     supportive_praise_flow <- get_flow_data(flow_name = supportive_praise, flow_type = "praise", include_archived_data = include_archived_data, date_to = "1970-01-01")
     supportive_calm_flow <- get_flow_data(flow_name = supportive_calm, flow_type = "calm", include_archived_data = include_archived_data, date_to = "1970-01-01")
     supportive_activities_flow <- get_flow_data(flow_name = supportive_activities, include_archived_data = include_archived_data, date_to = "1970-01-01")
