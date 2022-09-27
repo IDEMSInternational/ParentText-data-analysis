050c8bc054b67c9784a666a107876868928be10b
fagiothree <32491856+fagiothree@users.noreply.github.com>
2022-09-27 11:12:56 +0200

    Update shiny_app.R
    added variable country_ so the name of the dashboard is not South_Africa

--- R Code/shiny_app.R
+++ R Code/shiny_app.R
@@ -1,6 +1,7 @@
-country <- "South_Africa" # Jamaica, Philippines, South_Africa Malaysia
+country <- "South Africa" # Jamaica, Philippines, South Africa Malaysia
+country_ <- "South_Africa" # Jamaica, Philippines, South_Africa Malaysia
 source("Functions.R")
-source(paste0(country, "_dashboard_settings.R"))
+source(paste0(country_, "_dashboard_settings.R"))
 source("R Shiny Template.R")
 source("Data Cleaning.R")
 parenttext_shiny(country = country, date_from = default_date_from, date_to = default_date_to, include_archived_data = include_archived_data)
